48b39fdf433a55a7886a25e3adb0d9f9ea8aaf97
Emilia Walczak <emilia.walczak.87@wp.pl>
2018-07-10 13:43:46 +0200

    without rx

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,143 +1,81 @@
 // @flow
 import React from "react";
 import { Link } from "react-router-dom";
-import Rx from "rxjs/Rx";
-//import Subject from "rxjs/Subject"
-
+import PropTypes from 'prop-types'
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 
 class SearchBooks extends React.Component {
+
+
+static propTypes = {
+    books: PropTypes.array.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  }
+
   state = {
-    query: "",
-    books: [],
-    //searchInput:""
-};
+    query: '',
+    newBooks: [],
+    searchError: false
+  }
 
-searchInput: Rx.Subject<any>;
+  getBooks = (event) => {
 
-    constructor() {
-      super();
-      this.searchInput = new Rx.Subject();
-      this.searchInput.debounceTime(400).subscribe(param => {
-        this.fireSearchBook(param);
-      });
-    }
+    const query = event.target.value.trim()
+    this.setState({ query: query })
 
-  updateQuery = (query: string) => {
-    this.setState({
-      query: query
-    });
+
     if (query) {
-      this.searchInput.next(query);
-    } else {
-      this.setState({
-        books: []
-      });
-    }
-  };
+      BooksAPI.search(query, 20).then((books) => {
+        books.length > 0 ?  this.setState({newBooks: books, searchErr: false }) : this.setState({ newBooks: [], searchErr: true })
+      })
 
-  updateBooks(books: any) {
-    const verifiedBooks = books.map(book => {
-      book.shelf = "none";
-      this.props.booksOnShelf.forEach(bookOnShelf => {
-        if (book.id === bookOnShelf.id) {
-          book.shelf = bookOnShelf.shelf;
-        }
-      });
-      return book;
-    });
-    this.setState({
-      books: verifiedBooks
-    });
-  }
-
-  fireSearchBook(query: string) {
-    BooksAPI.search(query, 20).then(
-      response => {
-        if (response.error) {
-          this.setState({
-            books: []
-          });
-        } else {
-          this.updateBooks(response);
-        }
-      },
-      error => {
-        console.log("error ocurred");
-      }
-    );
-  }
-
-  updateBookOnSearch(book: any, shelf: string) {
-    let temp = this.state.books;
-    const bookToUpdate = temp.filter(t => t.id === book.id)[0];
-    bookToUpdate.shelf = shelf;
-    this.setState({
-      books: temp
-    });
-    this.props.onChangeShelf(book, shelf);
+  } else this.setState({newBooks: [], searchError: false })
   }
 
   render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/" className="close-search">
-            Close
-          </Link>
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              value={this.state.query}
-              onChange={event => this.updateQuery(event.target.value)}
-            />
+
+    const { query, newBooks, searchError } = this.state
+    const { books, changeShelf } = this.props
+
+      return (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link className="close-search"  to="/">Close</Link> //goes back to the page
+            <div className="search-books-input-wrapper">
+              <input type="text"
+                placeholder="Search by title or author"
+                value= { query }
+                onChange={ this.getBooks } /> //shows books when input provided
+            </div>
           </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-            {this.state.books.map(book =>
-              <li key={book.id} className="book">
-                <div className="book-top">
-                  <div
-                    className="book-cover"
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: "url(" + book.imageLinks.thumbnail + ")"
-                    }}
-                  />
-                  <div className="book-shelf-changer">
-                    <select
-                      value={book.shelf}
-                      onChange={e => {
-                        this.updateBookOnSearch(book, e.target.value);
-                      }}
-                    >
-                      <option value="none" disabled>
-                        Move to...
-                      </option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
+          <div className="search-books-results">
+            {newBooks.length > 0 && (
+              <div>
+                <div className=''>
+                  <h3>Search returned { newBooks.length } books </h3> //number of books
+                </div>
+                <ol className="books-grid">
+                  {newBooks.map((book) => (
+                    <book
+                      book={ book }
+                      books={ books }
+                      key={ book.id }
+                      changeShelf={ changeShelf }
+                    />
+                  ))}
+                </ol>
+              </div>
+            )}
+            { searchError  && (
+              <div>
+                <div className=''>
+                  <h3>Search returned no books</h3> //when no books found
                   </div>
                 </div>
-                <div className="book-title">
-                  {book.title}
-                </div>
-                {book.authors &&
-                  <div className="book-authors">
-                    {book.authors[0]}
-                  </div>}
-              </li>
             )}
-          </ol>
+          </div>
         </div>
-      </div>
-    );
-  }
+      )}
 }
 export default SearchBooks
